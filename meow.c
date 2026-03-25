9bcc3b1e3bf477ab8a0bf5327f9c7bc84e04d2c8
lux <luxvanderzel@outlook.com>
2026-03-25 12:54:50 +0100

    commit

--- meow.c
+++ meow.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
+ 
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+ 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+ 
+typedef float f32;
+typedef double f64;
+ 
+typedef i8 b8;
+typedef i32 b32;
 
+typedef struct {
+    f32 x;
+    f32 y;
+} vec2f;
+ 
+int main() {
+    vec2f v = {
+        .y = 1
+    };
 
+    printf("vector = < %f, %f >\n", v.x, v.y);
 
-
-void meow(int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("meow\n");
-    }
-}
-
-int main(void)
-{
-    meow(3);
     return 0;
-}
+};
